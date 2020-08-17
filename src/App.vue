d7a0b402eb182fa6fcc69aa42158b561f571023b
Momeemt <momiimt@gmail.com>
2020-08-17 15:43:58 +0900

    Install Vue-Router

--- src/App.vue
+++ src/App.vue
@@ -46,19 +46,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({
-  name: 'App',
-
-  components: {},
-
-  data: () => ({
-    date: {}
-  }),
-
-  methods: {}
-})
-</script>
